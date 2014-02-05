97acfe08c2db8ab5ded64b5fb537ce24d20a2c70
Blazej Pindelski <bpindelski@dundee.ac.uk>
2014-02-05 16:06:49 +0000

    ExecutorService implementation.

--- ConnectionThread.java
+++ ConnectionThread.java
@@ -1,4 +1,12 @@
-class ConnectionThread implements Runnable {
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ConnectionThread implements Callable<String> {
 
 	private omero.client client;
 
@@ -15,29 +23,52 @@ class ConnectionThread implements Runnable {
 		this.client = new omero.client(host, port);
 	}
 
-	public void run() {
+	public String call() {
 		try {
 			client.createSession(user, pass);
 		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
+			return e.getMessage();
 		} finally {
 			client.closeSession();
 		}
+		return null;
 	}
 
 	public static void main(String[] args) {
-		String host = args[0];
-		int port = Integer.parseInt(args[1]);
-		String user = args[2];
-		String pass = args[3];
+		final ExecutorService executorService1,executorService2;
+		Set<Callable<String>> callables1 = new HashSet<Callable<String>>();
+		Set<Callable<String>> callables2 = new HashSet<Callable<String>>();
+		String host = "localhost";
+		int port = 4064;
+		String user = "root";
+		String pass = "omero";
 
-		for (int i=0; i<10000; ++i) {
-			new Thread(new ConnectionThread(host, port, user, pass)).start();
-			if (i%1000 == 0) {
-				System.out.println(i);
-			}
+		if (args.length > 3) {
+			host = args[0];
+			port = Integer.parseInt(args[1]);
+			user = args[2];
+			pass = args[3];
+		}
+
+		for (int i=0; i<900; ++i) {
+			callables1.add(new ConnectionThread(host, port, user, pass));
+		}
+		for (int i=0; i<900; ++i) {
+			callables2.add(new ConnectionThread(host, port, user, pass));
+		}
+
+
+		executorService1 = Executors.newFixedThreadPool(900);
+		executorService2 = Executors.newFixedThreadPool(900);
+
+		try {
+			List<Future<String>> futures1 = executorService1.invokeAll(callables1);
+			List<Future<String>> futures2 = executorService2.invokeAll(callables2);
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		} finally {
+			executorService2.shutdown();
+			executorService1.shutdown();
 		}
 	}
-
 }
